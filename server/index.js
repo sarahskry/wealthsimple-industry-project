2fb340726e22a6265e2c3fafb29a8045a7504481
Sarah Skry <sarah.skrydstrup@gmail.com>
2024-05-30 17:29:32 -0400

    initial setup for server side

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
