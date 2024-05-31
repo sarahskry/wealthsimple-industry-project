fe7caaccedc70129ab618061a9faad4a96009022
rellagant <barbara.cs@gmail.com>
2024-05-30 22:53:21 -0400

    created footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.scss";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer__text">© Wealthsimple Inc Industry Project 2024. All Rights Reserved.</p>
+        </footer>
+    )
+}
+
